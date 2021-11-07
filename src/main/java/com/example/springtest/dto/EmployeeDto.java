0bdeebcd5f14a708f8ccf82bcab7de6041b5e28e
Gabichus <e_victor97@mail.ru>
2021-11-07 17:15:43 +0200

    Add Get/Set to Name and Role to EmployeeDto

--- src/main/java/com/example/springtest/dto/EmployeeDto.java
+++ src/main/java/com/example/springtest/dto/EmployeeDto.java
@@ -5,5 +5,19 @@ public class EmployeeDto{
     private String name;
     private String role;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
